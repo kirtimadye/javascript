297761f54a03070b796fcc97e6f34bdcec8a2702
kirtimadye <shindekirti2014@gmail.com>
2023-05-07 21:48:04 +0530

    Example of JS
    Example of JS

---
+++ hello.js
@@ -0,0 +1,15 @@
+console.log("Rahitya...........");
+console.log("Surekha");
+console.log("Address-Circuit House,Kolhapur")
+var text="Kirti";
+var text="Pallu";   //Variable Update
+var firstName = "Savya";
+console.log(text);
+console.log(firstName);
+var age=90;          //Variable Declaration
+console.log(age);    //Variable Initialization
+var age;
+console.log(age);
+var age1;
+console.log(age1);
+console.log();

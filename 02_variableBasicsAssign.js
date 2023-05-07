a1f8865030372fa2fa2653702b37e78c5a7682aa
kirtimadye <shindekirti2014@gmail.com>
2023-05-08 00:36:35 +0530

    Create 02_variableBasicsAssign.js
    Create 02_variableBasicsAssign.js

---
+++ 02_variableBasicsAssign.js
@@ -0,0 +1,30 @@
+console.log("Examples of Primitive Data Types 1.Number 2.String 3.Boolean");
+console.log("-------------------------1-String----------------------------");
+var HTML="Tim Berners-Lee";
+console.log("HTML is invented by-",HTML,"Type-",typeof HTML);
+var CSS="Hakon Wium Lie";
+console.log("CSS is invented by-",CSS,"Type-",typeof CSS);
+var javaScript="Brendan Eich"
+console.log("JavaScript is invented by-",javaScript,"Type-",typeof javaScript);
+console.log("-------------------------2-Number----------------------------");
+var year=1993;
+console.log("HTML First Version Year-",year,"Type-",typeof year);
+var year=1996;
+console.log("CSS First Version Year-",year,"Type-",typeof year);
+var yesr=1995;
+console.log("JavaScript First Version Year",year,"Type-",typeof year);
+console.log("-------------------------1-Boolean----------------------------");
+var text=true;
+console.log("Can we hide content using the comment?",text,"Type-",typeof text);
+var text=false;
+console.log("Are the negative values allowed in CSS padding property?",text,"Type-",typeof text);
+var text=true;
+console.log("Is JavaScript code inserted between <script> and </script> tags.",text,"Type-",typeof text);
+console.log("---------Difference between Null and Undefined---------");
+console.log("1-Null is an object.| 1-Undefined is a type itself.");
+console.log("2-Null indicates the absence of a value for a variable.| 2-Undefined indicates the absence of the variable itself.");
+console.log("3-Null is converted to zero (0) while performing primitive operations.| 3-Undefined is converted to NaN while performing primitive operations.");
+console.log("4-Null is an assignment value.| 4-Undefined is an not assignment value.");
+console.log("5-The null value is a primitive value which represents the null, empty, or non-existent reference.|5-The undefined value is a primitive value, which is used when a variable has not been assigned a value.");
+
+

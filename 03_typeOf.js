af8a1c1082998158ff8e5e03dbe60a0de5a48cdc
kirtimadye <shindekirti2014@gmail.com>
2023-05-08 00:35:25 +0530

    Create 03_typeOf.js
    Create 03_typeOf.js

---
+++ 03_typeOf.js
@@ -0,0 +1,22 @@
+console.log("------------------Use Variable,Data Type and Typeof operator-----------------");
+console.log("-----------1------------")
+var bankName;
+console.log("Before Variable bankName Initialization is-",bankName);
+var bankName="SBI Bank";
+console.log("After Variable bankName Initialization is-",bankName,"|","Type of data type ->bankName is-",typeof bankName);
+console.log("-----------2------------")
+var marks='90%';
+console.log("Student of Marks are-",marks,"|","Type of data type ->marks is-",typeof marks);
+console.log("-----------3------------")
+var isWorking;
+console.log("Before variable ->isWorking Initialization is-",isWorking);
+var isWorking=true;
+console.log("After variable ->isWorking Initialization is-",isWorking);
+console.log("-----------4-a------------")
+var totalCount="Hundred and Seven";
+console.log("Variable totalCount Value is-",totalCount,"|","Data type of variable totalCount is-",typeof totalCount);
+console.log("-----------4-b------------")
+var totalCount=107;
+console.log("Update Variable totalCount value is-",totalCount,"|","Data type of variable totalCount is-",typeof totalCount);
+
+

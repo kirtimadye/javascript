4ec7e4c18548238c806649648c436d368151c2a7
kirtimadye <shindekirti2014@gmail.com>
2023-05-07 21:47:06 +0530

    Primitive data type
    Primitive data type

---
+++ 01_primitiveTypes.js
@@ -0,0 +1,60 @@
+//Primitive Type
+//1.Number
+var age=31;
+var weight=78;
+var height=6.1;
+var speckNumber=0.50;
+
+console.log(age,weight,height,speckNumber);
+
+var typeofAge = typeof age;
+console.log(typeofAge);
+
+console.log(typeof speckNumber);
+console.log(typeof height);
+console.log(typeof weight);
+
+
+console.log("Age:",age);
+console.log("Weight:",weight);
+console.log("Height:",height);
+console.log(speckNumber);
+
+//2.Boolean--->true or false
+var isMarried = true;
+var knowAngular = true;
+var isSoftwareEngineer = true;
+var isBachelor = false;
+
+console.log(isMarried);
+console.log(knowAngular);
+console.log(isSoftwareEngineer);
+console.log(isBachelor);
+
+var typeofisMarried = typeof isMarried;
+console.log(typeofisMarried);
+
+
+// 3. String---->Sequence of character enclosed in ' ' or " "
+var firstName = "Bill";
+var position = 'Microsoft Co-founder'; 
+console.log(typeof firstName);
+var typeOfFirstName = typeof firstName;
+var city = '';
+console.log("City",typeof city);
+console.log("Type of variable ->firstName",typeOfFirstName
+);
+console.log("=======");
+console.log(typeof position);
+console.log("Type of variable ->position",typeof position);
+
+//2.Special Data type
+//1.Undefined
+var address;
+console.log("Address:",address);
+console.log("Type of variable address is",typeof address);
+
+//2.null
+var pin = null;
+console.log("Value of variable ->pin:",pin);
+console.log("Type of variable pin is",typeof pin);

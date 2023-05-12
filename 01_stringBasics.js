004e2ba95deae990c85c9fd4473b96240619cbf4
kirtimadye <shindekirti2014@gmail.com>
2023-05-12 21:31:48 +0530

    Create String file
    Create String file

---
+++ 01_stringBasics.js
@@ -0,0 +1,24 @@
+console.log("----------String Basics Assignments----------");
+console.log("----------1----------");
+function stringBasics()
+{
+    console.log("My dream company is TCS - Tata Consultancy Services");
+}
+stringBasics();
+console.log("-----------2.1----------");
+var hobby1="Programming";
+var hobby2="Writing";
+var hobby3="Travelling";
+console.log("My Hobbies are: ",hobby1,hobby2,hobby3);
+console.log("----------2.2------------");
+function myHobbies()
+{
+    var totalCharacter=(hobby1.length+hobby2.length+hobby3.length);
+    console.log("Sum the total number of characters = ",totalCharacter);
+    // hobby1.length;
+    // hobby2.length;
+    // hobby3.length;
+
+}
+myHobbies();
+

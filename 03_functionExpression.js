e87ad82b72e0547de408caef5026cd90a07a4f8b
kirtimadye <shindekirti2014@gmail.com>
2023-05-13 08:41:29 +0530

    Function Expression
    Function Expression

---
+++ 03_functionExpression.js
@@ -0,0 +1,12 @@
+console.log("------13/05/23----");
+function multyply(one,two,three)
+{
+    console.log(``);
+}
+multyply(2,2,2);
+var multiplication = function (one, two, three) {
+    var result = one*two*three;
+    return `Multiplication is: ${result}`;
+}
+var result = multiplication(2, 2, 2 );
+console.log(result);

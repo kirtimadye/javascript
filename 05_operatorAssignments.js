da3bce3ff225bf6b3fc1535cb4761ca1546bab4c
kirtimadye <shindekirti2014@gmail.com>
2023-05-15 09:02:56 +0530

    Create Assignment Operator File
    Create Assignment Operator File

---
+++ 05_operatorAssignments.js
@@ -0,0 +1,30 @@
+console.log("------15/05/23--------");
+console.log(`Compound Addition operators`);
+var num1=10;
+var num2=20;
+num2+=num1; //num2=num2+num1;
+console.log(`Compound Addition=${num2}`);
+
+console.log(`Compound Subtraction operators`);
+var num1=10;
+var num2=20;
+num2-=num1; //num2=num2+num1;
+console.log(`Compound Subtraction=${num2}`);
+
+console.log(`Compound Multiplication operators`);
+var num1=10;
+var num2=20;
+num2*=num1; //num2=num2+num1;
+console.log(`Compound Multiplication=${num2}`);
+
+console.log(`Compound Division operators`);
+var num1=10;
+var num2=20;
+num2/=num1; //num2=num2+num1;
+console.log(`Compound Division=${num2}`);
+
+console.log(`Compound Modulus operators`);
+var num1=10;
+var num2=20;
+num2%=num1; //num2=num2+num1;
+console.log(`Compound Modulus=${num2}`);

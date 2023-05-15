5bf8df36ae54e298ff2a8a5acd0ed6cbc466758e
kirtimadye <shindekirti2014@gmail.com>
2023-05-15 09:01:12 +0530

    Create Arithmetic Operator File
    Create Arithmetic Operator File

---
+++ 05_operatorsArithmetic.js
@@ -0,0 +1,45 @@
+console.log(`--------15/05/23-------`);
+console.log(`Addition operators`);
+var num1=10;
+var num2=20;
+var result=num1+num2;
+console.log(`Addition of two number=${result}`);
+
+console.log(`Subtraction operators`);
+var num1=10;
+var num2=20;
+var result=num1-num2;
+console.log(`Subtraction of two number=${result}`);
+
+console.log(`Multiplication operators`);
+var num1=10;
+var num2=20;
+var result=num1*num2;
+console.log(`Multiplication of two number=${result}`);
+
+
+console.log(`Division operators`);
+var num1=10;
+var num2=20;
+var result=num1/num2;
+console.log(`Division of two number=${result}`);
+
+
+console.log(`Exponentiation operators`);
+var num1=10;
+var num2=20;
+var result=num1**num2;
+console.log(`Exponentiation of two number=${result}`);
+
+console.log(`Modulus operators`);
+var num1=20;
+var num2=3;
+var result=num1%num2;
+console.log(`Modulus of two number=${result}`);
+
+console.log(`Increment operators`);
+var num1=20;
+var num2=13;
+var result=num2%2;
+console.log(`Increment of two number=${result}`);
+

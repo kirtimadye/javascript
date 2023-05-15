1bee922ac42a740724b6422745726fb4f8f7a5a9
kirtimadye <shindekirti2014@gmail.com>
2023-05-15 09:04:47 +0530

    Create Assignment Operator File
    Create Assignment Operator File

---
+++ 01_operatorsComparison.js
@@ -0,0 +1,77 @@
+console.log(`-----------15/05/23----------`);
+console.log(`Equal == operators`);
+var num1=10;
+var num2=20;
+var result=num1==num2;
+console.log(`Equality check=${result}`);
+
+console.log(`Equal == operators`);
+var num1=10;
+var num2="10";
+var result=num1==num2;
+console.log(`Equality check=${result}`);
+
+console.log(`Equal == operators`);
+var num1=10;
+var num2="22";
+var result=num1==num2;
+console.log(`Equality check=${result}`);
+
+console.log(`Equal == operators`);
+var num1=10;
+var num2=20;
+var result=num1==num2;
+console.log(`Equality check=${result}`);
+
+console.log(`Explicit Conversion`);
+var num3="200";
+console.log(`Type of num1 ->${typeof num3}`);
+var num4=+num3;
+console.log(`Type of ${num4}->${typeof num4}`);
+
+var num3="Pune";
+console.log(`Type of num1 ->${typeof num3}`);
+var num4=+num3;
+console.log(`Type of ${num4}->${typeof num4}`);
+
+console.log(`Strictly Equal == operators`);
+var num1=10;
+var num2="10";
+var result=num1===num2;
+console.log(`Strictly Equality check=${result}`);
+
+console.log(`Not Equal == operators`);
+var num1=10;
+var num2="10";
+var result=num1!=num2;
+console.log(`Not Equality check=${result}`);
+
+console.log(`Strictly Not Equal !== operators`);
+var num1=10;
+var num2="10";
+var result=num1!==num2;
+console.log(`Strictly Not Equality check=${result}`);
+
+console.log(` Less than < operators`);
+var num1=10;
+var num2=20;
+var result=num1<num2;
+console.log(`Less than check=${result}`);
+
+console.log(` Greater than > operators`);
+var num1=10;
+var num2=20;
+var result=num1>num2;
+console.log(`Greater than check=${result}`);
+
+console.log(` Less than equal <= operators`);
+var num1=10;
+var num2=10;
+var result=num1<=num2;
+console.log(`Less than equal check=${result}`);
+
+console.log(` Greater than equal >= operators`);
+var num1=10;
+var num2=10;
+var result=num1>=num2;
+console.log(`Greater than equal check=${result}`);

a709cd1347e656889d13f213e0f6ece152b65204
kirtimadye <shindekirti2014@gmail.com>
2023-05-16 08:44:16 +0530

    Create ternary file
    Create ternary file

---
+++ 05_ternaryOperator.js
@@ -0,0 +1,24 @@
+console.log(`-----16/05/23-----`);
+
+var isAssignmentDone=true;
+var result = isAssignmentDone ? "You will get Job":"You will not get Job";
+console.log(result);
+
+var amarSSC=57;
+var amarHSC=89;
+var result=amarSSC>=55 && amarHSC>=60 ? "Allowed for interview":"Sorry,Not allowed";
+console.log(`${result}`);
+
+var kirtiAge=22;
+var result=kirtiAge>=18 ? "You are eligible for vote ":"Not try next time";
+console.log(`${result}`);
+
+var candidateName="Jenny";
+var age=23;
+var gender="male";
+var result=(age>=18 && gender=="female") || (age>=21 && gender=="male") ? "You are eligible":"You are not eligible";
+console.log(`${result}`);
+
+var num1=13;
+var result=num1%2==0 ? "Even number" : "Odd Number";
+console.log(`${result}`);

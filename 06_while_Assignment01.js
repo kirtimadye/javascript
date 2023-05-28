6278b84524886471387c5a97cdb1d9744d0d96a7
kirtimadye <shindekirti2014@gmail.com>
2023-05-28 12:14:25 +0530

    Create While Statement
    Create While Statement

---
+++ 06_while_Assignment01.js
@@ -0,0 +1,50 @@
+console.log(`1.WAP to print numbers from 5 to 15 by incrementing by 1`);
+var incNum=5;
+let newIncNum="";
+while (incNum<=15) {
+    
+    console.log(`Incrementing numbers are = ${incNum}`);
+    incNum++;  
+}
+console.log(`=============================================================`);
+console.log(`2.WAP to print numbers from 50 to 40 by decrementing by 1`);
+let decNum=50;
+while (decNum>=40) {
+    console.log(`Decrementing numbers are = ${decNum}`);
+    decNum--;
+}
+console.log(`=============================================================`);
+console.log(`3.WAP to find first 15 odd numbers`);
+let oddNum=1;
+while (oddNum<=15) {
+    console.log(`First 15 Odd numbers are = ${oddNum}`);
+    oddNum=oddNum+2;
+}
+console.log(`=============================================================`);
+console.log(`4.WAP to find first 10 even numbers`);
+let evenNum=0;
+while (evenNum<=10) {
+    console.log(`First 10 even numbers are = ${evenNum}`);
+    evenNum=evenNum+2;
+}
+console.log(`=============================================================`);
+console.log(`5.WAP to print table of 5 like-->5 10 15 20 25 ...... 50`);
+let tableFive=5;
+while (tableFive<=50) {
+    console.log(`Table of 5 = ${tableFive}`);
+    tableFive=tableFive+5;
+}
+console.log(`=============================================================`);
+console.log(`6.WAP to print table of 10 like 10 20 30 40 ..... 100`);
+let tableTen=10;
+while (tableTen<=100) {
+    console.log(`Table of 10 = ${tableTen}`);
+    tableTen=tableTen+10;
+}
+console.log(`=============================================================`);
+console.log(`7.WAP to print table of 10 in reverse order like -->100 90 80 70 10`);
+let revNum=100;
+while (revNum>=10) {
+    console.log(`Table of 10 in reverse order = ${revNum}`);
+    revNum=revNum-10;
+}
